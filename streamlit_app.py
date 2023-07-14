bc7690a2bcaef0ca8897cd1dd761d0c86d2b4203
Mark Gardiner <mark.david.gardiner@outlook.com>
2023-07-14 10:18:07 +0100

    test replace with text

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,4 +60,4 @@ def insert_row_snowflake(new_fruit):
 if streamlit.button('Add a Fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
-    streamlit.test(back_from_function)
+    streamlit.text(back_from_function)
